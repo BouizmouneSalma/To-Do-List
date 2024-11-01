b07477779608b26d311e29afffbfeae04ac178d3
BouizmouneSalma <salmabouizmoun@gmail.com>
2024-11-01 19:46:19 +0100

    faire addEventListener pour cacher le modal

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 const addTaskBtn = document.getElementById("task");
 const addForm = document.getElementById("task-form");
+const cancelBtn = document.getElementById("cancel");
+const toDoList = document.getElementById("to-do-list");
 const modal = document.getElementById("modal");
+const inProgressList = document.getElementById("in-progress-list");
+const doneList = document.getElementById("done-list");
+
+const toDoCount = document.getElementById("to-do-count");
+const inProgressCount = document.getElementById("in-progress-count");
+const doneCount = document.getElementById("done-count");
 
 addTaskBtn.addEventListener("click", () => {
-modal.classList.remove("hidden");
-addForm.classList.remove("hidden"); // Affiche le formulaire
+    modal.classList.remove("hidden"); // remove  hidde
+    addForm.classList.remove("hidden"); // Affiche le formulaire
 });
+
+cancelBtn.addEventListener("click", () => {
+    addForm.classList.add("hidden"); // Cache le formulaire
+    modal.classList.add("hidden"); // Cache le modal
+});
+
+
 
